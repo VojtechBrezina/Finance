31d5a57acf955270dff0290e9a26abf4bd9d049b
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2021-01-30 10:08:25 +0100

    Add project files.

---
+++ Finance/TitleScreen.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finance {
+	/// <summary>
+	/// Interaction logic for TitleScreen.xaml
+	/// </summary>
+	public partial class TitleScreen: UserControl {
+		public TitleScreen() {
+			InitializeComponent();
+		}
+	}
+}
