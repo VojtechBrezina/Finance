febb4dcf7ebde54358fdae6576d250d8aefe9562
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2021-02-06 11:09:16 +0100

    Začátek práce na vykreslování grafů.

---
+++ Finance/PlotControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finance {
+	/// <summary>
+	/// Ovládací prvek pro zobrazování nastavitelných grafů.
+	/// Pro některé konkrétní věci bude pohodlnější použít
+	/// třídy, které z něj dědí.
+	/// </summary>
+	public partial class PlotControl: UserControl {
+		/// <summary>
+		/// Barva pozadí je uložená v proměnné, pro případnou budoucí rimplementaci umožňující uživatelská nastavení.
+		/// </summary>
+		private Color backgroundColor = Colors.White;
+
+		public PlotControl() {
+			InitializeComponent();
+		}
+
+
+	}
+}
---
+++ Finance/PlotDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance {
+	/// <summary>
+	/// Třída pro poskytování dat ovládacímu prvku
+	/// <see cref="PlotControl"/>. Je zodpovědná za
+	/// optimalizaci přístupu k datům a předávání 
+	/// událostí popisujících jejich změny.
+	/// </summary>
+	abstract class PlotDataSource {
+		public event EventHandler DataChanged;
+		
+
+		protected virtual void OnDataChanged() {
+			DataChanged?.Invoke(this, new EventArgs());
+		}
+	}
+}
